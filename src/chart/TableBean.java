d855c706d9150c38b27d12c4f85bbe7d038d2756
weihong <neoweihong12@gmail.com>
2017-01-30 22:26:51 +0800

    30/1

--- src/chart/TableBean.java
+++ src/chart/TableBean.java
@@ -1,9 +1,11 @@
 package chart;
 
 import appointment.Appointment;
+import chart.DataValues;
 import appointment.AppointmentDAO;
 
 import java.io.Serializable;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +18,7 @@ import javax.faces.bean.ManagedBean;
 public class TableBean implements Serializable {
 
     private List<ColumnModel> columns;
-    private List<Appointment> appt;
+    private List<DataValues> dataValues;
 
     private String colName;
 
@@ -26,30 +28,57 @@ public class TableBean implements Serializable {
     }
 
     private void addTableView(){
-        appt = new ArrayList<Appointment>();
+        dataValues = new ArrayList<DataValues>();
         try {
+            //retrieve, process and add to arraylist
             AppointmentDAO dao = new AppointmentDAO();
+            String [] yearArr = {"2001", "2002","2003","2004","2005","2006","2007","2008",
+                    "2009","2010","2011","2012","2013","2014","2015","2016","2017"};
+            List<Appointment> apptList = dao.getAllAppointment();
+            Appointment appt = new Appointment();
+            int numAppt;
+            String temp = null;
+
+            for(int i = 0 ; i < yearArr.length; i++) {
+                numAppt = 0;
+                for (int j = 0; j < apptList.size(); j++) {
+                    appt = apptList.get(j);
+                    temp = appt.getDate().substring(6);
+                    if (temp.equals(yearArr[i])) {
+                        numAppt = numAppt + 1;
+                    }
+                }
+
+                DataValues dv = new DataValues(yearArr[i], numAppt, calPercent(numAppt, apptList.size()));
+                dataValues.add(dv);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //retrieve and add to arraylist
+
 
     }
 
     private void createDynamicColumns(){
         columns = new ArrayList<ColumnModel>();
-        columns.add(new ColumnModel("Types","type"));
+        columns.add(new ColumnModel("Year","year"));
         columns.add(new ColumnModel("Amount","amount"));
         columns.add(new ColumnModel("Percentage","percentage"));
     }
+    private String calPercent(int value , int total){
+        DecimalFormat df = new DecimalFormat("#0.00");
+        double percent = (double)value / total;
 
-    public List<Appointment> getAppt(){
-        return appt;
+        return df.format(percent);
     }
 
-    public void setTableView(List<Appointment> tableView){
-        this.appt = tableView;
+    public List<DataValues> getDataValues(){
+        return dataValues;
+    }
+
+    public void setTableView(List<DataValues> tableView){
+        this.dataValues = tableView;
     }
 
     public List<ColumnModel> getColumns(){
